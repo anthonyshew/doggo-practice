061156788197273369a5a66391778b981a6f57db
Anthony Shew <anthonyshew@gmail.com>
2022-05-27 07:15:04 -0700

    Woah, it's accessible on a public ip.

--- .pulumi/index.ts
+++ .pulumi/index.ts
@@ -1,7 +1,32 @@
 import * as awsx from "@pulumi/awsx";
 import * as aws from "@pulumi/aws";
+import * as docker from '@pulumi/docker'
 
-// Create a repository.
+// Create a docker repository.
 const repo = new awsx.ecr.Repository("my-repo");
 
-export const repoUrl = repo.repository.repositoryUrl;
+// const imageeeeeee = new awsx.ecr.Image("image", {
+//     repositoryUrl: repo.url,
+//     path: "./app",
+// })
+
+const image = awsx.ecr.buildAndPushImage("name", {
+  context:  "..",
+  dockerfile: "../apps/web/Dockerfile"
+})
+
+const cluster = new awsx.ecs.Cluster("default-cluster")
+
+const lb = new awsx.lb.ApplicationLoadBalancer("nginx-lb")
+
+const service = new awsx.ecs.FargateService('my-service', {
+  cluster,
+  taskDefinitionArgs: {
+    container: {
+      image: image,
+    }
+  }
+
+})
+
+export const lbDns = lb.loadBalancer.dnsName
