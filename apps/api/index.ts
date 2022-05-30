14eff75c05e2e9d32b6a0c6eb9376ac48854c281
Anthony Shew <anthonyshew@gmail.com>
2022-05-29 18:16:46 -0700

    Drop localstack.

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -2,6 +2,10 @@ import { randomUUID } from "crypto";
 import express from "express";
 import {
   PutObjectCommand,
+  PutBucketCorsCommand,
+  DeleteBucketCommand,
+  ListBucketsCommand,
+  CORSConfiguration,
   S3Client,
   CreateBucketCommand,
 } from "@aws-sdk/client-s3";
@@ -20,19 +24,71 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create-bucket", async (req, res) => {
-  const command = new CreateBucketCommand({ Bucket: bucketName });
-  const created = await client.send(command);
-  return res.json({ created });
+  try {
+    // Create new bucket and...
+    const command = new CreateBucketCommand({ Bucket: bucketName });
+    const created = client.send(command).then(async () => {
+      // ... set up CORS for it
+      const corsConfig: CORSConfiguration = {
+        CORSRules: [
+          {
+            AllowedHeaders: ["Authorization"],
+            AllowedMethods: [],
+            AllowedOrigins: ["*"],
+            ExposeHeaders: [],
+          },
+        ],
+      };
+
+      const corsCommand = new PutBucketCorsCommand({
+        Bucket: bucketName,
+        CORSConfiguration: corsConfig,
+      });
+      const corsPolicy = await client.send(corsCommand);
+      return res.json({ created, corsPolicy });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({ ...error });
+  }
+});
+
+app.post("/delete-bucket", async (req, res) => {
+  try {
+    const command = new DeleteBucketCommand({ Bucket: bucketName });
+    const deleted = client.send(command).then(() => {
+      return res.json({ deleted });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({ ...error });
+  }
+});
+
+app.get("/list-buckets", async (req, res) => {
+  try {
+    const command = new ListBucketsCommand({});
+    const buckets = await client.send(command);
+    return res.json({ buckets });
+  } catch (error) {
+    console.error(error);
+    return res.json({ ...error });
+  }
 });
 
 app.get("/s3-url", async (req, res) => {
-  const command = new PutObjectCommand({
-    Bucket: bucketName,
-    Key: randomUUID(),
-  });
+  try {
+    const command = new PutObjectCommand({
+      Bucket: bucketName,
+      Key: randomUUID(),
+    });
 
-  const url = await getSignedUrl(client, command, { expiresIn: 30 });
-  return res.json({ url });
+    const url = await getSignedUrl(client, command, { expiresIn: 30 });
+    return res.json({ url, bucketName });
+  } catch (error) {
+    console.error(error);
+    return res.json({ ...error });
+  }
 });
 
 app.listen(port, () => {
