f466cc060ad954ac8153e1f2ee724ae941ddef2c
Anthony Shew <anthonyshew@gmail.com>
2022-05-29 18:16:17 -0700

    More s3 endpoints.

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,27 +1,63 @@
 import { Button } from "ui";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export default function Web() {
-  const [data, setData] = useState({});
+  const [bucketName, setBucketName] = useState("");
+  const [signedUrl, setSignedUrl] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:5000")
+  const listBuckets = () => {
+    fetch("http://localhost:5000/list-buckets")
       .then((res) => res.json())
-      .then((res) => setData(res));
-  }, []);
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.error(err));
+  };
 
   const createBucket = () => {
     fetch("http://localhost:5000/create-bucket", { method: "POST" })
       .then((res) => res.json())
-      .then((res) => console.log(res))
+      .then((res) => {
+        if (res["$fault"]) return;
+        setBucketName(res);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const deleteBucket = () => {
+    fetch("http://localhost:5000/delete-bucket", { method: "POST" })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res["$fault"]) return;
+        setBucketName("");
+      })
       .catch((err) => console.error(err));
   };
 
   const getSignedUrl = () => {
     fetch("http://localhost:5000/s3-url")
       .then((res) => res.json())
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res);
+        setSignedUrl(res.url);
+      })
       .catch((err) => console.error(err));
+  };
+
+  const uploadPic = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    const data = new FormData();
+
+    if (files) {
+      data.append("file", files[0]);
+      fetch(signedUrl, {
+        method: "POST",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+        .catch((err) => console.error(err));
+    }
   };
 
   return (
@@ -29,9 +65,13 @@ export default function Web() {
       <h1>Web</h1>
       <p>{process.env.TEST_ME}</p>
       <p>{process.env.NEXT_PUBLIC_TEST_ME}</p>
-      {JSON.stringify(data)}
-      <Button onClick={() => createBucket()} text="Create Bucket" />
+      <p>{bucketName}</p>
+      <Button onClick={listBuckets} text="List Buckets" />
+      <Button onClick={createBucket} text="Create Bucket" />
+      <Button onClick={deleteBucket} text="Delete Bucket" />
       <Button onClick={() => getSignedUrl()} text="Get signed url" />
+      <input type="file" onChange={uploadPic} />
+      <Button onClick={() => console.log("")} text="Present pic" />
     </div>
   );
 }
